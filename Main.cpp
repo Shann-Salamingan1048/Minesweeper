b821a5c2e37879a3492bcab6e72b0812aa472d53
Shann Delfin C. Salamingan IIII <134818470+Shann-Salamingan1048@users.noreply.github.com>
2024-04-26 15:47:09 +0800

    Remove console

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,18 @@
+
 #include <iostream>
 #include "Game.h"
-int main()
+// to show console
+//int main()
+// Project Properties : Linker > SubSystem >  Console(/ SUBSYSTEM:CONSOLE)
+int WinMain() // use WinMain() to not show console and change the 
+// Project Properties: Linker > SubSystem, from Console(/SUBSYSTEM:CONSOLE) to Windows(/SUBSYSTEM:WINDOWS)
 {
+	
 	Game game;
 	
 	while (game.isRunning())
 	{
+	
 		game.update();
 		game.render();
 	}
